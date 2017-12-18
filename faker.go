ab2d7bfa8e202ba3e3b1f8542f789bf343406baa
Arinze Okeke <trent9002@gmail.com>
2017-12-18 13:01:14 +0100

    firstname and lastname finished

--- faker.go
+++ faker.go
@@ -2,8 +2,31 @@ package faker
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"time"
 
-	"github.com/Arinzeokeke/tr"
+	"../../tr"
+)
+
+var (
+	definitions = map[string][]string{
+		"name":         []string{"first_name", "last_name", "prefix", "suffix", "gender", "title", "male_first_name", "female_first_name", "male_middle_name", "female_middle_name", "male_last_name", "female_last_name"},
+		"address":      []string{"city_prefix", "city_suffix", "street_suffix", "county", "country", "country_code", "state", "state_abbr", "street_prefix", "postcode", "direction", "direction_abbr"},
+		"company":      []string{"adjective", "noun", "descriptor", "bs_adjective", "bs_noun", "bs_verb", "suffix"},
+		"lorem":        []string{"words"},
+		"hacker":       []string{"abbreviation", "adjective", "noun", "verb", "ingverb", "phrase"},
+		"phone_number": []string{"formats"},
+		"finance":      []string{"account_type", "transaction_type", "currency", "iban", "credit_card"},
+		"internet":     []string{"avatar_uri", "domain_suffix", "free_email", "example_email", "password"},
+		"commerce":     []string{"color", "department", "product_name", "price", "categories"},
+		"database":     []string{"collation", "column", "engine", "type"},
+		"system":       []string{"mimeTypes"},
+		"date":         []string{"month", "weekday"},
+		"title":        []string{},
+		"separator":    []string{},
+	}
 )
 
 // Fake Object
@@ -15,7 +38,7 @@ type Fake struct {
 
 // Faker interface
 type Faker interface {
-	Name() Namer
+	Name() Name
 }
 
 //New creates new faker
@@ -38,10 +61,23 @@ func New() (*Fake, error) {
 }
 
 // Name returns name
-func (f *Fake) Name() Namer {
+func (f *Fake) Name() Name {
 	return Name{
 		f:      f,
 		engine: f.engine,
 	}
+}
 
+func random(i int) int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	return r.Intn(i)
+}
+
+func directoryExists(dir string) bool {
+	dir, _ = filepath.Abs(dir)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
--- name.go
+++ name.go
@@ -1,6 +1,6 @@
 package faker
 
-import "github.com/Arinzeokeke/tr"
+import "../../tr"
 import "strings"
 
 const (
@@ -10,7 +10,7 @@ const (
 // Namer interface
 type Namer interface {
 	FirstName() string
-	// LastName() string
+	LastName() string
 	// FindName() string
 	// JobTitle() string
 	// Prefix() string
@@ -24,12 +24,60 @@ type Namer interface {
 // Name struct
 type Name struct {
 	engine *tr.Engine
-	f      Faker
+	f      *Fake
 }
 
 // FirstName returns first name
 func (n *Name) FirstName() string {
+	if directoryExists(n.f.defaultLocale+"/"+prefix+"/male_first_name") &&
+		directoryExists(n.f.defaultLocale+"/"+prefix+"/female_first_name") {
 
-	firsties := strings.Split(n.engine.Tr(prefix+"/first_name"), "\n")
-	return firsties[0]
+		// TODO give gender choice
+		if !directoryExists(n.f.defaultLocale + "/" + prefix + "/first_name") {
+			firsties, err := getList(n.engine, prefix+"/female_first_name", n.f.defaultLocale)
+			if err != nil {
+				panic(err)
+			}
+			return firsties[random(len(firsties))]
+		}
+
+	}
+	firsties, err := getList(n.engine, prefix+"/first_name", n.f.defaultLocale)
+	if err != nil {
+		panic(err)
+	}
+	return firsties[random(len(firsties))]
+}
+
+// LastName returns last name
+func (n *Name) LastName() string {
+	if directoryExists(n.f.defaultLocale+"/"+prefix+"/male_last_name") &&
+		directoryExists(n.f.defaultLocale+"/"+prefix+"/female_last_name") {
+
+		// TODO give gender choice
+		if !directoryExists(n.f.defaultLocale + "/" + prefix + "/last_name") {
+			firsties, err := getList(n.engine, prefix+"/female_last_name", n.f.defaultLocale)
+			if err != nil {
+				panic(err)
+			}
+			return firsties[random(len(firsties))]
+		}
+
+	}
+	lasties, err := getList(n.engine, prefix+"/last_name", n.f.defaultLocale)
+	if err != nil {
+		panic(err)
+	}
+	return lasties[random(len(lasties))]
+}
+
+func getList(n *tr.Engine, q string, d string) ([]string, error) {
+	w, err := n.Tr(q)
+	if err != nil {
+		w, err = n.Lang(d).Tr(q)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(w, "\n"), nil
 }
