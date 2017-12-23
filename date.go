f29d55f0dbabc99700f65efda94939942c515141
Arinze Okeke <trent9002@gmail.com>
2017-12-23 13:31:57 +0100

    recent function on date interface

--- date.go
+++ date.go
@@ -8,11 +8,11 @@ import (
 type Dater interface {
 	Past(years int) time.Time
 	Future(years int) time.Time
-	// Between
-	// Recent
-	// Soon
-	// Month
-	// Weekday
+	Between
+	Recent(days int) time.Time
+	Soon(days int) time.Time
+	Month(abbr, context bool) string
+	Weekday(abbr, context bool) string
 }
 
 // Date struct
@@ -40,12 +40,22 @@ func (d *Date) Future(years int) time.Time {
 	return time.Unix(tNano/1000, 0)
 }
 
+// Recent returns a recent time
+func (d *Date) Recent(days int) time.Time {
+	t := time.Now()
+	tNano := t.Unix() * 1000
+
+	tNano -= int64(randomIntRange(1000, days*24*3600*1000))
+
+	return time.Unix(tNano/1000, 0)
+}
+
 // Soon returns a soon time
 func (d *Date) Soon(days int) time.Time {
 	t := time.Now()
 	tNano := t.Unix() * 1000
 
-	tNano -= int64(randomIntRange(1000, days*24*3600*1000))
+	tNano += int64(randomIntRange(1000, days*24*3600*1000))
 
 	return time.Unix(tNano/1000, 0)
 }
@@ -59,9 +69,25 @@ func (d *Date) Month(abbr, context bool) string {
 		t = "/wide"
 	}
 
-	if context && directoryExists(d.DefaultLocale+"/"+datePrefix+t+"_context") {
+	if context && directoryExists(d.DefaultLocale+"/"+datePrefix+"/month"+t+"_context") {
 		t += "_context"
 	}
 
-	return d.pick(datePrefix + t)
+	return d.pick(datePrefix + "/month" + t)
+}
+
+// Weekday returns a  time weekday
+func (d *Date) Weekday(abbr, context bool) string {
+	var t string
+	if abbr {
+		t = "/abbr"
+	} else {
+		t = "/wide"
+	}
+
+	if context && directoryExists(d.DefaultLocale+"/"+datePrefix+"/weekday"+t+"_context") {
+		t += "_context"
+	}
+
+	return d.pick(datePrefix + "/weekday" + t)
 }
