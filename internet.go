5f775952d65448bd614040448a9fecedc6f88a2a
OLumide Falomo <olumightyfalomo@gmail.com>
2018-09-10 13:27:02 +0100

    Implement internet function(s)
    DomainName()
    DomainWord()
    Email()
    ExampleEmail()
    URL()
    IP()
    IPv6()
    Color()
    Mac()
    UserAgent()
    
    Add util functions for slugifying and generating random hash

--- internet.go
+++ internet.go
@@ -1,15 +1,30 @@
 package faker
 
-import "strings"
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"strings"
+
+	"github.com/corpix/uarand"
+)
 
 // Internetier interface
 type Internetier interface {
 	Avatar() string
+	Color(baseRed255, baseGreen255, baseBlue255 float64) string
+	DomainName() string
 	DomainSuffix() string
-	Email() string
-	ExampleEmail() string
+	DomainWord() string
+	Email(firstName, lastName, provider string) string
+	ExampleEmail(firstName, lastName string) string
+	IP() string
+	IPv6() string
+	Mac(sep string) string
 	Protocol() string
-	UserName() string
+	URL() string
+	UserAgent() string
+	UserName(firstName, lastName string) string
 }
 
 // Internet struct
@@ -22,15 +37,109 @@ func (i *Internet) Avatar() string {
 	return i.pick(internetPrefix + "/avatar_uri")
 }
 
+// Color Generates a random hexadecimal color.
+// based on awesome response : http://stackoverflow.com/questions/43044/algorithm-to-randomly-generate-an-aesthetically-pleasing-color-palette
+func (i *Internet) Color(baseRed255, baseGreen255, baseBlue255 float64) string {
+	red := math.Floor((float64(random(256)) + baseRed255) / 2)
+	green := math.Floor((float64(random(256)) + baseRed255) / 2)
+	blue := math.Floor((float64(random(256)) + baseRed255) / 2)
+
+	redStr := fmt.Sprintf("%02X", int(red))
+	greenStr := fmt.Sprintf("%02X", int(green))
+	blueStr := fmt.Sprintf("%02X", int(blue))
+
+	return "#" + redStr + greenStr + blueStr
+}
+
+// DomainName Generates a random domain name.
+func (i *Internet) DomainName() string {
+	return i.Internet().DomainWord() + "." + i.Internet().DomainSuffix()
+}
+
 // DomainSuffix Generates a random domain suffix.
 func (i *Internet) DomainSuffix() string {
 	return i.pick(internetPrefix + "/domain_suffix")
+}
+
+// DomainWord Generates a random domain word.
+func (i *Internet) DomainWord() string {
+	re := regexp.MustCompile(`([\\~#&*{}/:<>?|\"'])`)
+	return strings.ToLower(re.ReplaceAllString(i.Name().FirstName(), ""))
+}
+
+// Email Generates a valid email address
+func (i *Internet) Email(firstName, lastName, provider string) string {
+	if provider == "" {
+		provider = i.pick(internetPrefix + "/free_email")
+	}
+	return slugify(strings.ToLower(i.Internet().UserName(firstName, lastName))) + "@" + provider
+}
+
+// ExampleEmail Provides an email example
+func (i *Internet) ExampleEmail(firstName, lastName string) string {
+	provider := i.pick(internetPrefix + "/example_email")
+	return i.Internet().Email(firstName, lastName, provider)
+}
+
+// IP Generates a random IP
+func (i *Internet) IP() string {
+	result := []string{}
+
+	for i := 0; i < 4; i++ {
+		result[i] = string(random(255))
+	}
+
+	return strings.Join(result, ".")
+}
+
+// IPv6 Generates a random IPv6 address
+func (i *Internet) IPv6() string {
+	result := []string{}
+
+	for i := 0; i < 8; i++ {
+		result[i] = randHash()
+	}
+
+	return strings.Join(result, ".")
+}
+
+// Mac Generates a random mac address.
+func (i *Internet) Mac(sep string) string {
+	mac := ""
+	validSep := ":"
+
+	// if the client passed in a different separator than `:`,
+	// we will use it if it is in the list of acceptable separators (dash or no separator)
+	acceptedSep := strslice{"-", ""}
+	if acceptedSep.indexOf(sep) != -1 {
+		validSep = sep
+	}
+
+	for i := 0; i < 12; i++ {
+		mac += strings.TrimSpace(fmt.Sprintf("%2x", random(15)))
+
+		if (i%2 == 1) && (i != 11) {
+			mac += validSep
+		}
+	}
+
+	return mac
 }
 
 // Protocol Randomly generates http or https
 func (i *Internet) Protocol() string {
 	protocols := []string{"http", "https"}
 	return selectElement(protocols)
+}
+
+// URL Generates a random url.
+func (i *Internet) URL() string {
+	return i.Internet().Protocol() + "://" + i.Internet().DomainName()
+}
+
+// UserAgent Generates a random user agent
+func (i *Internet) UserAgent() string {
+	return uarand.GetRandom()
 }
 
 // UserName Generates a username based on one of several patterns
--- util.go
+++ util.go
@@ -4,12 +4,15 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/Arinzeokeke/tr"
 )
+
+type strslice []string
 
 func replaceSymbols(s string) string {
 	alpha := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
@@ -71,7 +74,36 @@ func getList(n *tr.Engine, q, d string) ([]string, error) {
 	return strings.Split(w, "\n"), nil
 }
 
+// Find the first index at which an element exists in the array
+func (slice strslice) indexOf(searchElement string) int {
+	for index, val := range slice {
+		if val == searchElement {
+			return index
+		}
+	}
+	return -1
+}
+
 // Return a random elemnet from an array
 func selectElement(arr []string) string {
 	return arr[random(len(arr))]
+}
+
+func slugify(word string) string {
+	re := regexp.MustCompile(" ")
+	rex := regexp.MustCompile(`[^\w\.\-]+`)
+
+	replaceHyphen := re.ReplaceAllString(word, "-")
+	replaceBlank := rex.ReplaceAllString(replaceHyphen, "")
+
+	return replaceBlank
+}
+
+// Generate a 4-digit hash
+func randHash() string {
+	hash := ""
+	for i := 0; i < 4; i++ {
+		hash += selectElement([]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"})
+	}
+	return hash
 }
