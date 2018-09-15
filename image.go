8839d15b9ce5686124cedc0b955da3fdeaf21ccd
OLumide Falomo <olumightyfalomo@gmail.com>
2018-09-15 19:47:31 +0100

    Add Categorized Methods for image
    #Too many to list

--- image.go
+++ image.go
@@ -8,6 +8,20 @@ import (
 type Imager interface {
 	Avatar() string
 	ImageURL(width, height int, category string) string
+	DataURL(width, height int) string
+	Abstract(width, height int) string
+	Animals(width, height int) string
+	Business(width, height int) string
+	Cats(width, height int) string
+	City(width, height int) string
+	Food(width, height int) string
+	Nightlife(width, height int) string
+	Fashion(width, height int) string
+	People(width, height int) string
+	Nature(width, height int) string
+	Sports(width, height int) string
+	Technics(width, height int) string
+	Transport(width, height int) string
 }
 
 // Image struct
@@ -31,4 +45,76 @@ func (i *Image) ImageURL(width, height int, category string) string {
 	url = url + "?" + strconv.Itoa(random(999))
 
 	return url
+}
+
+// DataURL Returns a dataurl format
+func (i *Image) DataURL(width, height int) string {
+	rawPrefix := `data:image/svg+xml;charset=UTF-8,`
+	svgString := `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" baseProfile="full" width="` + strconv.Itoa(width) + `" height="` + strconv.Itoa(height) + `"> <rect width="100%" height="100%" fill="grey"/>  <text x="0" y="20" font-size="20" text-anchor="start" fill="white">` + strconv.Itoa(width) + `x` + strconv.Itoa(height) + `</text> </svg>`
+	return rawPrefix + svgString
+}
+
+// Abstract Returns an image in the Abstract category
+func (i *Image) Abstract(width, height int) string {
+	return i.Image().ImageURL(width, height, "abstract")
+}
+
+// Animals Returns an image in the Animals category
+func (i *Image) Animals(width, height int) string {
+	return i.Image().ImageURL(width, height, "animals")
+}
+
+// Business Returns an image in the Business category
+func (i *Image) Business(width, height int) string {
+	return i.Image().ImageURL(width, height, "business")
+}
+
+// Cats Returns an image in the Cats category
+func (i *Image) Cats(width, height int) string {
+	return i.Image().ImageURL(width, height, "cats")
+}
+
+// City Returns an image in the City category
+func (i *Image) City(width, height int) string {
+	return i.Image().ImageURL(width, height, "city")
+}
+
+// Food Returns an image in the Food category
+func (i *Image) Food(width, height int) string {
+	return i.Image().ImageURL(width, height, "food")
+}
+
+// Nightlife Returns an image in the Nightlife category
+func (i *Image) Nightlife(width, height int) string {
+	return i.Image().ImageURL(width, height, "nightlife")
+}
+
+// Fashion Returns an image in the Fashion category
+func (i *Image) Fashion(width, height int) string {
+	return i.Image().ImageURL(width, height, "fashion")
+}
+
+// People Returns an image in the People category
+func (i *Image) People(width, height int) string {
+	return i.Image().ImageURL(width, height, "people")
+}
+
+// Nature Returns an image in the Nature category
+func (i *Image) Nature(width, height int) string {
+	return i.Image().ImageURL(width, height, "nature")
+}
+
+// Sports Returns an image in the Sports category
+func (i *Image) Sports(width, height int) string {
+	return i.Image().ImageURL(width, height, "sports")
+}
+
+// Technics Returns an image in the Technics category
+func (i *Image) Technics(width, height int) string {
+	return i.Image().ImageURL(width, height, "technics")
+}
+
+// Transport Returns an image in the Transport category
+func (i *Image) Transport(width, height int) string {
+	return i.Image().ImageURL(width, height, "transport")
 }
