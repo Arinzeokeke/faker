4753ecb8018a1e0945d6275b12e9c0b39e6583d1
OLumide Falomo <olumightyfalomo@gmail.com>
2018-09-15 19:20:35 +0100

    Add Methods For image
    Avatar()
    ImageURL()

--- image.go
+++ image.go
@@ -1,10 +1,34 @@
 package faker
 
+import (
+	"strconv"
+)
+
 // Imager interface
 type Imager interface {
+	Avatar() string
+	ImageURL(width, height int, category string) string
 }
 
 // Image struct
 type Image struct {
 	*Fake
+}
+
+// Avatar Generates a URL for an avatar.
+func (i *Image) Avatar() string {
+	return i.Internet().Avatar()
+}
+
+// ImageURL Generates an image url
+func (i *Image) ImageURL(width, height int, category string) string {
+	protocol := "http://"
+	w := strconv.Itoa(width)
+	h := strconv.Itoa(height)
+
+	url := protocol + "lorempixel.com/" + w + "/" + h + "/" + category
+
+	url = url + "?" + strconv.Itoa(random(999))
+
+	return url
 }
